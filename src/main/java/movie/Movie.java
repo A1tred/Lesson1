982568bcaf64a3adab90457ee4434982ccdc55fc
A1tred <socold800@gmail.com>
2022-06-06 17:44:40 +0300

    Task "Movie"

---
+++ src/main/java/movie/Movie.java
@@ -0,0 +1,38 @@
+package movie;
+
+public class Movie {
+
+    String title;
+    int duration;
+    String director;
+
+    public Movie(String title, int duration, String director) {
+        this.title = title;
+        this.duration = duration;
+        this.director = director;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+}
